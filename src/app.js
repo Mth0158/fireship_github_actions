1c27b8b8f7d22165d293d0aa265ccbd7c78b72b6
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-04-10 13:47:11 +0200

    hello world

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    return days[date.getDay()];
-}
+  return days[date.getDay()];
+};
 
+console.log("Hello world!");
 
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
-
+  document.getElementById("day").innerText = dayOfTheWeek();
+} catch (err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
